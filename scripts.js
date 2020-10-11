f19e8bc9297ddd9c0a33401245e59de64aad92e3
Leah O'Gorman <leah.p.ogorman@gmail.com>
2020-10-11 11:23:06 -0400

    worked on javascript

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
-var dayForcast = document.getElementById("dayForcast")
+var dayForcast = document.getElementById("dayForcastParagraph")
 var fiveDayForcast = document.getElementById("fiveDayForcast")
-function displayCity(){
-    var apiKey = "0ad01036f2ab800c0683594b20735973"
+var apiKey = "0ad01036f2ab800c0683594b20735973"
+var icon = document.getElementById("icon")
 
-    var location = "Atlanta";
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-    
-    $ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response)
-        $(dayForcast).append(
-            response
-        )
-    })
-}
+var cityName = "";
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=0ad01036f2ab800c0683594b20735973`;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+      let iconCode = response.weather[0].icon;
+      let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+      console.log(queryURL)
+      console.log(response)
+      console.log(iconUrl)
+      $(".city").html("<h1>" + response.name)
+      $('#icon').attr('src', iconUrl);
+
+    });
